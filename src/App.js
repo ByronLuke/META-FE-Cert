4ed56fb75e96f585b2e6ff3e4d34eb78dcb5a353
ByronLuke <byronpc18@icloud.com>
2024-05-29 14:57:03 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
-function Heading(){
-  return(
-    <h1>This is an h1 heading</h1>
-  )
-}
+import Heading from './Heading';
 
 function App() {
-  return (
-    <div className="App">
-    <Heading/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
 }
 
 export default App;
