ed2f58276633a66eec7a4cda4c651abd53e8fca1
ByronLuke <byronpc18@icloud.com>
2024-06-01 14:15:21 -0500

    react components section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import Heading from './Heading';
+import Heading from './components/Heading';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <Heading />
+        <Heading firstName="Bob"/>
+        <Heading firstName="Lee"/>
+        <h1>Task: Add three Card elements</h1>
+        <Card h2="First card's h2" h3="First card's h3"/>
+        <Card h2="Second card's h2" h3="Second card's h3"/>
+        <Card h2="Third card's h2" h3="Third card's h3"/>
     </div>
   );
-}
-
+} 
+  
 export default App;
