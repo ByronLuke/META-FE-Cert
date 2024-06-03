22c85896adc7527f5b3cfe6dc07f1e2879add7c9
ByronLuke <byronpc18@icloud.com>
2024-06-03 17:38:22 -0500

    propsdrilling and counter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
+import React, { useState } from "react";
+import Fruits from "./components/Fruits";
+import FruitsCounter from "./components/FruitsCounter";
 
 function App() {
-    function handleClick(){
-        let randomNum = Math.floor(Math.random()*3)+1;
-        console.log(randomNum)
-        let userInput = prompt('type a number');
-        alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
-    }
-  return (
-    <div>
-      <h1>Task: Add a button and handle a click event</h1>
-      <button onClick={handleClick}>Guess the number between 1 and 3. </button>
+    const [fruits]=useState([
+        {fruitName: "apple", id: 1},
+        {fruitName: "apple", id: 2},
+        {fruitName: "plum", id: 3}
+    ])
+  return (
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits}/>
+      <FruitsCounter fruits={fruits}/>
     </div>
-    
-  );
-} 
-  
+  );
+};
+
 export default App;
---
+++ src/components/FruitsCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function FruitsCounter({fruits}){
+    return(
+        <h1>Counter: {fruits.length}</h1>
+    )
+}
+
+export default FruitsCounter
